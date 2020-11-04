10520d77ed204a27ee95aac4341e62230a1a55fd
EdJoJob <edjojob@gmail.com>
2020-11-04 10:44:54 +1000

    Have better debugging logs

---
+++ debug/emit_context_handler.py
@@ -0,0 +1,14 @@
+from talon import Context, actions
+
+ctx = Context()
+
+
+@ctx.action_class("core")
+class Actions:
+    def run_talon_script(ctx, script, m):
+        with ctx:
+            print(f"CONTEXT: {ctx}")
+            print(f"SCRIPT: {script}")
+            print(f"COMMAND: {m}")
+            script.run(actions, namespace=m)
+
